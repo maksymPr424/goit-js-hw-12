441c9c3d43332ead8a58d6b24b1f03a20df830b6
maksymPr424 <m.prykhodko424@gmail.com>
2024-05-27 13:17:21 +0200

    Refactoryng from then to async/await

--- src/main.js
+++ src/main.js
@@ -23,40 +23,41 @@ iziToast.settings({
   position: 'topRight',
 });
 
-const addImages = (searchText, page = 1) => {
-  getArr(searchText, page)
-    .then(response => {
-      if (response.data.hits.length === 0) {
-        iziToast.warning({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      }
-      const totalPages = Math.ceil(response.data.totalHits / 15);
-      if (page > totalPages) {
-        loadMoreBtn.style.display = 'none';
-        return iziToast.error({
-          message: "We're sorry, but you've reached the end of search results.",
-        });
-      }
-      loadMoreBtn.style.display = 'block';
+const addImages = async (searchText, page = 1) => {
+  try {
+    const response = await getArr(searchText, page);
 
-      renderImages(response.data.hits, gallery);
-    })
-    .catch(error => console.error('Error', error))
-    .finally(() => {
-      form.reset();
-      loader.style.display = 'none';
-
-      const options = gallery.children.galleryItem.getBoundingClientRect();
-      window.scrollBy({
-        top: options.height * 2,
-        behavior: 'smooth',
+    if (response.data.hits.length === 0) {
+      iziToast.warning({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
       });
+    }
+    const totalPages = Math.ceil(response.data.totalHits / 15);
+    if (page > totalPages) {
+      loadMoreBtn.style.display = 'none';
+      return iziToast.error({
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+    loadMoreBtn.style.display = 'block';
+
+    renderImages(response.data.hits, gallery);
+  } catch (error) {
+    error => console.error('Error', error);
+  } finally {
+    form.reset();
+    loader.style.display = 'none';
+
+    const options = gallery.children.galleryItem.getBoundingClientRect();
+    window.scrollBy({
+      top: options.height * 2,
+      behavior: 'smooth',
     });
+  }
 };
 
-const submitSearchForm = e => {
+const submitSearchForm = async e => {
   e.preventDefault();
   gallery.innerHTML = '';
   loadMoreBtn.style.display = 'none';
@@ -67,7 +68,7 @@ const submitSearchForm = e => {
   addImages(searchText);
 };
 
-const loadMore = e => {
+const loadMore = async e => {
   page += 1;
   addImages(searchText, page);
 };
